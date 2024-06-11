1a16a6cbdf4819e6141377c4d6aed27825d59a9f
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-06-11 14:26:59 -0400

    remove debug #1
    Signed-off-by: Alejandro Ouslan <alejandro.ouslan@upr.edu>

--- app.py
+++ app.py
@@ -120,4 +120,4 @@ def update_figure(selected_year):
     return fig
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host="0.0.0.0", port=7050)
+    app.run_server(debug=False, host="0.0.0.0", port=7050)
