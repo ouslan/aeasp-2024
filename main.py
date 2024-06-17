cd47273bd702dab2079a455c72fd010b9ed4a160
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-06-17 14:05:23 -0400

    added boilerplate files #15
    Signed-off-by: Alejandro Ouslan <alejandro.ouslan@upr.edu>

--- main.py
+++
@@ -1,9 +0,0 @@
-from src.data.data_pull import download_file
-
-def main() -> None:
-    url = "https://www2.census.gov/ces/movs/movs_st_main2005.csv"
-    filename = "data/raw/movs_st_main2005.csv"
-    download_file(url, filename)
-
-if __name__ == "__main__":
-    main()
