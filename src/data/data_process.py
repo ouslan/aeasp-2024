3fb447c3dd5046ea1bd785ae174fd29ed98710f6
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-07-07 07:37:38 -0400

    fixed bug regarding male acs agrigate
    Signed-off-by: Alejandro Ouslan <alejandro.ouslan@upr.edu>

--- src/data/data_process.py
+++ src/data/data_process.py
@@ -19,7 +19,8 @@ class DataProcess(DataPull):
             pl.Series("year", [], dtype=pl.Date),
             pl.Series("state", [], dtype=pl.Int64),
             pl.Series("PUMA", [], dtype=pl.Int64),
-            pl.Series("avg_time", [], dtype=pl.Float64),
+            pl.Series("PWGTP", [], dtype=pl.Float64),
+            pl.Series("avg_time", [], dtype=pl.Int64),
             pl.Series("sex", [], dtype=pl.Int32),
             pl.Series("race", [], dtype=pl.String),
         ]
@@ -28,39 +29,23 @@ class DataProcess(DataPull):
         for file in os.listdir("data/raw"):
             if file.startswith("acs"):
                 original = pl.read_parquet(f"data/raw/{file}")
-                for sex in [0, 1, 3]:
-                    for race in [
-                        "RACAIAN",
-                        "RACASN",
-                        "RACBLK",
-                        "RACNUM",
-                        "RACWHT",
-                        "RACSOR",
-                        "HISP",
-                        "ALL",
-                    ]:
+                for sex in [1, 2, 3]:
+                    for race in ["RACAIAN","RACASN","RACBLK","RACNUM","RACWHT","RACSOR","HISP","ALL",]:
                         df = original
                         if not sex == 3:
                             df = df.filter(pl.col("SEX") == sex)
                         if not race == "ALL":
                             df = df.filter(pl.col(race) == 1)
                         df = df.select("year", "state", "PUMA", "PWGTP", "JWMNP")
-                        df = df.with_columns(
-                            total_time=(pl.col("PWGTP") * pl.col("JWMNP"))
-                        )
+                        df = df.with_columns(total_time=(pl.col("PWGTP") * pl.col("JWMNP")))
                         df = df.group_by("year", "state", "PUMA").agg(
-                            pl.col("PWGTP", "total_time").sum()
-                        )
-                        df = df.select(
-                            "year",
-                            "state",
-                            "PUMA",
-                            (pl.col("total_time") / pl.col("PWGTP")).alias("avg_time"),
-                        )
+                                                                      pl.col("PWGTP", "total_time").sum())
+                        df = df.select("year","state", "PUMA", "PWGTP",
+                                       (pl.col("total_time") / pl.col("PWGTP")).alias("avg_time"),
+                                      )
                         df = df.with_columns(
-                            pl.col("year").cast(pl.String).str.to_date("%Y"),
-                            sex=sex,
-                            race=pl.lit(race),
+                                             sex=pl.lit(sex),
+                                             race=pl.lit(race),
                         )
                         acs = pl.concat([acs, df], how="vertical")
         return acs
@@ -69,7 +54,7 @@ class DataProcess(DataPull):
         columns = ['linear_id', 'year', 'geometry']
         empty_data = {col: [] for col in columns}
 
-        for year in range(2010, 2011):
+        for year in range(2012, 2019):
             first_file_processed = False
             if os.path.exists(f"data/processed/roads_{year}.parquet"):
                 continue
@@ -84,8 +69,7 @@ class DataProcess(DataPull):
                         roads_df = gdf.copy()  # Initialize roads_df with the first valid gdf
                         first_file_processed = True
                     else:
-                        roads_df = pd.concat([roads_df, gdf], ignore_index=True)
-                        
+                        roads_df = pd.concat([roads_df, gdf], ignore_index=True) 
                     print("\033[0;36mINFO: \033[0m" + f"Finished processing roads for {file}")
             
             roads_df.to_parquet(f"data/processed/roads_{year}.parquet")
